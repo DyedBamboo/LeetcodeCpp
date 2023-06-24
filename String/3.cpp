6ebeaa8047ac256bbe04c85bf72ddad197cc4dda
LHH <1977808250@qq.com>
2023-06-24 12:13:53 +0800

    剑指Offer 05.替换空格

--- String/3.cpp
+++ String/3.cpp
@@ -6,3 +6,24 @@
  * @FilePath: \LeetcodeCpp\String\3.cpp
  * @版权声明 RanZhuJun
  */
+#include <iostream>
+#include <string>
+using namespace std;
+
+string replaceSpace(string s) {
+    string res;
+    for (auto c : s) {
+        if (c == ' ')
+            res += "%20";
+        else
+            res += c;
+    }
+    return res;
+}
+
+int main() {
+    string s = "We are happy.";
+    string res = replaceSpace(s);
+    cout << res << endl;
+    return 0;
+}
