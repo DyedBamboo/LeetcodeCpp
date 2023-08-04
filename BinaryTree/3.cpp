512085b4b9550f408d665eb65ae4953360fb2334
DyedBamboo <1977808250@qq.com>
2023-08-04 15:02:20 +0800

    leetcode 94 二叉树的中序遍历

---
+++ BinaryTree/3.cpp
@@ -0,0 +1,51 @@
+/**
+ * @file 3.cpp
+ * @author RanZhuJun
+ * @date 2023-08-04
+ * @brief leetcode 94 二叉树的中序遍历
+ * 
+ * @copyright RanZhuJun 2023
+ * 
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+void traversal(TreeNode* cur, vector<int>& res) {
+    if (cur == nullptr)
+        return;
+    traversal(cur->left, res);
+    res.push_back(cur->val);
+    traversal(cur->right, res);
+}
+
+vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> res;
+    traversal(root, res);
+    return res;
+}
+
+int main() {
+    vector<int> vec = {1, 2, 3, 4, 5, 6, 7};
+    TreeNode* root = new TreeNode(vec[0]);
+    root->left = new TreeNode(vec[1]);
+    root->right = new TreeNode(vec[2]);
+    root->left->left = new TreeNode(vec[3]);
+    root->left->right = new TreeNode(vec[4]);
+    root->right->left = new TreeNode(vec[5]);
+    root->right->right = new TreeNode(vec[6]);
+    vector<int> res = inorderTraversal(root);
+    for (int num : res) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
