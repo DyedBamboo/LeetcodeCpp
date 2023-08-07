5ea456bcba73403f5e7f6354504b5f01c7866745
DyedBamboo <1977808250@qq.com>
2023-08-07 19:44:13 +0800

    leetcode 107 二叉树的层序遍历Ⅱ

---
+++ BinaryTree/5.cpp
@@ -0,0 +1,66 @@
+/**
+ * @file 5.cpp
+ * @author RanZhuJun
+ * @date 2023-08-07
+ * @brief leetcode 107 二叉树的层序遍历Ⅱ
+ *
+ * @copyright RanZhuJun 2023
+ *
+ */
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+vector<vector<int>> levelOrder2(TreeNode* root) {
+    vector<vector<int>> res;
+    queue<TreeNode*> que;
+    if (root != nullptr)
+        que.push(root);
+    while (!que.empty()) {
+        int size = que.size();
+        vector<int> vec;
+        for (int i = 0; i < size; ++i) {
+            TreeNode* node = que.front();
+            que.pop();
+            vec.push_back(node->val);
+            if (node->left)
+                que.push(node->left);
+            if (node->right)
+                que.push(node->right);
+        }
+        res.push_back(vec);
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+int main() {
+    vector<int> vec = {1, 2, 3, 4, 5, 6, 7};
+    TreeNode* root = new TreeNode(vec[0]);
+    root->left = new TreeNode(vec[1]);
+    root->right = new TreeNode(vec[2]);
+    root->left->left = new TreeNode(vec[3]);
+    root->left->right = new TreeNode(vec[4]);
+    root->right->left = new TreeNode(vec[5]);
+    root->right->right = new TreeNode(vec[6]);
+
+    vector<vector<int>> res = levelOrder2(root);
+    for (auto vec : res) {
+        for (auto num : vec) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return 0;
+}
