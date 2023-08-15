151714e8a4aefb06e47c1e8628ace144e15daa1a
DyedBamboo <1977808250@qq.com>
2023-08-15 14:57:10 +0800

    leetcode 组合总数IV

--- DynamicProgramming/13.cpp
+++ DynamicProgramming/13.cpp
@@ -14,11 +14,11 @@
 using namespace std;
 
 int combinationSum4(vector<int>& nums, int target) {
-    vector<int> dp(target + 1,0);
+    vector<int> dp(target + 1, 0);
     dp[0] = 1;
-    for(int i = 0; i <= target; ++i) {
-        for(int j = 0; j < nums.size(); ++j) {
-            if(i - nums[j] >= 0 && dp[i] < INT_MAX - dp[i - nums[j]]) {
+    for (int i = 0; i <= target; ++i) {          // 遍历背包
+        for (int j = 0; j < nums.size(); ++j) {  // 遍历物品
+            if (i - nums[j] >= 0 && dp[i] < INT_MAX - dp[i - nums[j]]) {
                 dp[i] += dp[i - nums[j]];
             }
         }
