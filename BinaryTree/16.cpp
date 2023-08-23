148ac1477d939169d0c28d1ac32dfd9624d7e4e2
DyedBamboo <1977808250@qq.com>
2023-08-23 19:18:00 +0800

    leetcode 257 二叉树的所有路径

---
+++ BinaryTree/16.cpp
@@ -0,0 +1,67 @@
+/**
+ * @file 16.cpp
+ * @author RanZhuJun
+ * @date 2023-08-15
+ * @brief leetcode 257 二叉树的所有路径
+ *
+ * @copyright RanZhuJun 2023
+ *
+ */
+
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+vector<string> binaryTreePaths(TreeNode* root) {
+    stack<TreeNode*> treeStk;
+    stack<string> pathStk;
+    vector<string> result;
+    if (root == nullptr)
+        return result;
+    treeStk.push(root);
+    pathStk.push(to_string(root->val));
+    while (!treeStk.empty()) {
+        TreeNode* node = treeStk.top();
+        treeStk.pop();
+        string path = pathStk.top();
+        pathStk.pop();
+        if (node->left == nullptr && node->right == nullptr) {
+            result.push_back(path);
+        }
+        if (node->right) {
+            treeStk.push(node->right);
+            pathStk.push(path + "->" + to_string(node->right->val));
+        }
+
+        if (node->left) {
+            treeStk.push(node->left);
+            pathStk.push(path + "->" + to_string(node->left->val));
+        }
+    }
+    return result;
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->right = new TreeNode(5);
+
+    vector<string> res = binaryTreePaths(root);
+    for (auto str : res) {
+        cout << str << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
